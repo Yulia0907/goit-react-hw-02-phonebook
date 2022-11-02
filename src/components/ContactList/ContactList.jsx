ce0b9b51da01c605ee9bac5988380f1892dcc8bb
Yulia Sukhonenko <103435385+Yulia0907@users.noreply.github.com>
2022-11-02 14:30:27 +0200

    ДЗ №2 - 2 задание
    Книга контактов готов

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { ContactListItem } from '../ContactListItem/ContactListItem';
+import { ListContacts } from './ContactList.styled';
+
+export const ContactList = ({ contacts, handleDelete }) => {
+  return (
+    <ListContacts>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <ContactListItem
+            handleDelete ={handleDelete}
+            key={id}
+            id={id}
+            name={name}
+            number={number}
+          />
+        );
+      })}
+    </ListContacts>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  handleDelete: PropTypes.func.isRequired,
+};
